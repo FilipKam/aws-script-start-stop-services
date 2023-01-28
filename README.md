7b2ffc3938d8fd0d73246593f2956c985212a62d
Filip Kamenar <filip@kamenar.tech>
2023-01-28 23:12:38 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# aws-script-start-stop-services
+# ABOUT
+
 Script running in lambda functions for starting/stopping EC2, ECS and RDS resources.
+
+## HOW TO USE SCRIPT IN LAMBDA FUNCTION
+
+- Create lambda function manually or using CloudFormation script infra.yaml
+- Lambda must have permissions to start, stop, list and describe:
+  - EC2
+  - ECS
+  - RDS
+- For testing lambda event input JSON is defined like:
+
+```
+{
+  "action": "stop"
+}
+```
+
+## HOW TO USE SCRIPT LOCALLY
+
+### start resources
+
+```python3 script.py -a start```
+
+### stop resources
+
+```python3 script.py -a stop```
---
+++ lambda_src/lambda_function.py
@@ -0,0 +1,162 @@
+import boto3
+import logging
+import json
+from botocore.exceptions import ClientError
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class AWSManager:
+    def __init__(self):
+        # create clients for EC2, ECS and RDS
+        self.ec2_client = boto3.client("ec2")
+        self.ecs_client = boto3.client("ecs")
+        self.rds_client = boto3.client("rds")
+
+    def start_ec2_instances(self):
+        # start all EC2 instances
+        try:
+            response = self.ec2_client.describe_instances()
+            if response["Reservations"]:
+                for reservation in response["Reservations"]:
+                    for instance in reservation["Instances"]:
+                        instance_id = instance["InstanceId"]
+                        self.ec2_client.start_instances(InstanceIds=[instance_id])
+                        logger.info(f"Started EC2 instance {instance_id}")
+            else:
+                raise Exception("No EC2 instances found.")
+        except Exception as e:
+            logger.error(e)
+
+    def stop_ec2_instances(self):
+        # stop all EC2 instances
+        try:
+            response = self.ec2_client.describe_instances()
+            if response["Reservations"]:
+                for reservation in response["Reservations"]:
+                    for instance in reservation["Instances"]:
+                        instance_id = instance["InstanceId"]
+                        self.ec2_client.stop_instances(InstanceIds=[instance_id])
+                        logger.info(f"Stopped EC2 instance {instance_id}")
+            else:
+                raise Exception("No EC2 instances found.")
+        except Exception as e:
+            logger.error(e)
+
+    def start_rds_db(self):
+        # start all RDS instances
+        try:
+            response = self.rds_client.describe_db_instances()
+            if response["DBInstances"]:
+                for db_instance in response["DBInstances"]:
+                    db_instance_id = db_instance["DBInstanceIdentifier"]
+                    try:
+                        self.rds_client.start_db_instance(
+                            DBInstanceIdentifier=db_instance_id
+                        )
+                        logger.info(f"Started RDS instance {db_instance_id}")
+                    except ClientError as e:
+                        if e.response["Error"]["Code"] == "InvalidDBInstanceState":
+                            logger.error(
+                                f"RDS instance {db_instance_id} cannot be started as it is not in a valid state"
+                            )
+                        else:
+                            raise
+            else:
+                raise Exception("No RDS instances found.")
+        except Exception as e:
+            logger.error(e)
+
+    def stop_rds_db(self):
+        # stop all RDS instances
+        try:
+            response = self.rds_client.describe_db_instances()
+            if response["DBInstances"]:
+                for db_instance in response["DBInstances"]:
+                    db_instance_id = db_instance["DBInstanceIdentifier"]
+                    try:
+                        self.rds_client.stop_db_instance(
+                            DBInstanceIdentifier=db_instance_id
+                        )
+                        logger.info(f"Stopped RDS instance {db_instance_id}")
+                    except ClientError as e:
+                        if e.response["Error"]["Code"] == "InvalidDBInstanceState":
+                            logger.error(
+                                f"RDS instance {db_instance_id} cannot be stopped as it is not in a valid state"
+                            )
+                        else:
+                            raise
+            else:
+                raise Exception("No RDS instances found.")
+        except Exception as e:
+            logger.error(e)
+
+    def start_ecs_tasks(self):
+        # start all ECS tasks
+        try:
+            response = self.ecs_client.list_tasks()
+            for task_arn in response["taskArns"]:
+                self.ecs_client.start_task(taskArn=task_arn)
+                logger.info(f"Started ECS task {task_arn}")
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ClusterNotFoundException":
+                logger.error("No ECS clusters present, nothing to stop")
+            elif e.response["Error"]["Code"] == "AccessDeniedException":
+                logger.error("Access denied to ECS, check your credentials")
+            else:
+                logger.error(f"An error occurred while stopping ECS tasks: {e}")
+
+    def stop_ecs_tasks(self):
+        # stop all ECS tasks
+        try:
+            response = self.ecs_client.list_tasks()
+            for task_arn in response["taskArns"]:
+                self.ecs_client.stop_task(taskArn=task_arn)
+                logger.info(f"Stopped ECS task {task_arn}")
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ClusterNotFoundException":
+                logger.error("No ECS clusters present, nothing to stop")
+            elif e.response["Error"]["Code"] == "AccessDeniedException":
+                logger.error("Access denied to ECS, check your credentials")
+            else:
+                logger.error(f"An error occurred while stopping ECS tasks: {e}")
+
+
+def lambda_handler(event, context):
+    """
+    Main function that is executed when the Lambda is triggered
+    """
+    # create an instance of the AWSManager class
+    manager = AWSManager()
+    # get the action from the event
+    action = event["action"]
+
+    if action == "start":
+        logger.info("Starting all AWS resources")
+        # manager.start_ec2_instances()
+        manager.start_rds_db()
+        manager.start_ecs_tasks()
+        return {"statusCode": 200, "body": "All resources started"}
+
+    elif action == "stop":
+        logger.info("Stopping all AWS resources")
+        manager.stop_ec2_instances()
+        manager.stop_ecs_tasks()
+        manager.stop_rds_db()
+        return {"statusCode": 200, "body": "All resources stopped"}
+
+
+if __name__ == "__main__":
+    # Example how to start script: python3 script.py -a start
+    # Example how to start script: python3 script.py -a stop
+    logging.info("Script started from local machine")
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--action", help="stop or start", type=str)
+    args = parser.parse_args()
+    event = {"action": args.action}
+
+    lambda_handler(event, None)
